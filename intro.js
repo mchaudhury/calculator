cc0137d12a54d324757df6fcded5310cf70f74dc
mchaudhury <mushsalcha@gmail.com>
2021-08-23 13:57:26 -0500

    Initial commit

--- intro.js
+++ intro.js
@@ -12,3 +12,22 @@ console.log(`I am ${age} years old`);
 const isCool = true;
 
 
+const readline = require("readline");
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+reader.question("Enter a number: ", function(answer){
+
+    if (+answer === 12){
+        console.log("That is correct!")
+    } else {
+        console.log("Not correct")
+    }
+
+    reader.close();
+    
+})
+

4e27d7f6e13259e5043f3f2bbfae4d866e4ee1a9
mchaudhury <mushsalcha@gmail.com>
2021-08-23 13:19:41 -0500

    version 1.0

---
+++ intro.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+
+const firstName = "Adam";
+let age = "25";
+console.log(firstName,age);
+
+let favRobot = "Voltron";
+let favColor = `blue`;
+
+console.log(`I am ${age} years old`);
+
+const isCool = true;
+
+
